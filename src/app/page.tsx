575356811dd98940cc0e6beb58e6a0f4f9bf0b2d
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-08-30 00:42:48 +0500

    salom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,10 @@
 import { ProductType } from "@/types/product.types";
-
-// Misol mahsulot obyekti
-const exampleProduct: ProductType = {
-  id: 1,
-  title: "Example Product 1",
-  image: "/next.svg",
-  price: 99.99,
-  description: "This is an example product description.",
-  category: "Electronics",
-  rating: {
-    rate: 4.5,
-    count: 100,
-  },
-};
-
-const Hom = ({ product }: { product: ProductType }) => {
+type ProductCardProps = {
+  product: ProductType;
+}
+const Hom  = ({product}:ProductCardProps) => {
   return (
-    <div>
-      <img src={product.image} alt="rasim" />
-      {product.id}. {product.title}
-    </div>
-  );
-};
-
-const Home = () => {
-  return (
-    <div>
-      <Hom product={exampleProduct} />
-    </div>
-  );
-};
-
-export default Home;
+    <div>Hom page</div>
+  )
+}
+export default Hom
--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -8,15 +8,12 @@ const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <div className="p-5 basis-[30%] shadow-md rounded-lg">
       <img
-        src={product.image} // To'g'ri manzil bilan rasm yuklanadi
+        src={product.image}
         alt={product.title}
-        className="w-full h-[300px] object-contain"
+        className="w-full  h-[300px] object-contain"
       />
-      <h2 className="font-bold">{product.id}. {product.title}</h2>
-      <p className="text-lg font-semibold">${product.price}</p>
-      <p className="text-sm text-gray-500">{product.description}</p>
+      {product.id}. {product.title}
     </div>
   );
 };
-
 export default ProductCard;
